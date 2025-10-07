e8d249f4a9ce3f0fee820f1a4c85de497edf3efe
Fernando Furtado <carrilho.furtado@gmail.com>
2025-10-07 13:51:13 -0300

    resolucao e graficos de 20 e 40 MHz

--- metricas/10mhz/10mhz.py
+++ metricas/10mhz/10mhz.py
@@ -2,21 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Carrega os atrasos do Sym e do RB (em ns)
-sym = np.loadtxt("delays_sym.txt")
-rb = np.loadtxt("delays_rb.txt")
+sym = np.loadtxt("delays_sym_ofdma_10MHz.txt")
+rb = np.loadtxt("delays_rb_ofdma_10MHz.txt")
+otdma = np.loadtxt("delays_5gl_tdma_10MHz.txt")
+gl_ofdma = np.loadtxt("delays_5gl_ofdma_10MHz.txt")
 
 # Converte para milissegundos
 sym_ms = sym / 1e6
 rb_ms = rb / 1e6
+otdma_ms = otdma / 1e6
+gl_ofdma_ms = gl_ofdma / 1e6
 
 # Cálculos
-print(f"Sym-OFDMA: Média = {np.mean(sym_ms):.3f} ms | Desvio = {np.std(sym_ms):.3f} ms")
 print(f"RB-OFDMA:  Média = {np.mean(rb_ms):.3f} ms | Desvio = {np.std(rb_ms):.3f} ms")
+print(f"5GL-OFDMA: Média = {np.mean(gl_ofdma_ms):.3f} ms | Desvio = {np.std(gl_ofdma_ms):.3f} ms")
+print(f"Sym-OFDMA: Média = {np.mean(sym_ms):.3f} ms | Desvio = {np.std(sym_ms):.3f} ms")
+print(f"5GL-TDMA:  Média = {np.mean(otdma_ms):.3f} ms | Desvio = {np.std(otdma_ms):.3f} ms")
 
 # Gráfico comparativo
 plt.figure()
-plt.boxplot([sym_ms, rb_ms], labels=["Sym-OFDMA", "RB-OFDMA"])
-plt.ylabel("Delay (ms)")
-plt.title("Comparação de Atraso PDCP entre Sym-OFDMA e RB-OFDMA")
+plt.boxplot([rb_ms, gl_ofdma_ms, sym_ms, otdma_ms], labels=["RB-OFDMA", "5GL-OFDMA", "Sym-OFDMA", "5GL-TDMA"])
+plt.ylabel("Latency (ms)")
+plt.title("10 MHz") #Packet PDCP Delay
 plt.grid(True)
 plt.show()
